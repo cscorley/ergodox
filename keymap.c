a540db677603ea9a973b08c16cdfbd2d4a17eb93
Christopher Corley <cscorley@ua.edu>
2017-01-26 09:09:31 -0500

    reord commas, remove esc on lctl

--- keymap.c
+++ keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------------+------+------+------+------+-------------|       |------+------+------+------+------+------+------------|
  * | Tab  Media |   Q  |   W  |   E  |   R  |   T  |   [  |       |  ]   |   Y  |   U  |   I  |   O  |   P  | \|   Media |
  * |------------+------+------+------+------+------|      |       |      |------+------+------+------+------+------------|
- * | LCtrl  ESC |   A  |   S  |   D  |   F  |   G  |------|       |------|   H  |   J  |   K  |   L  |  ;   | '"  Symbol |
+ * | LCtrl      |   A  |   S  |   D  |   F  |   G  |------|       |------|   H  |   J  |   K  |   L  |  ;   | '"  Symbol |
  * |------------+------+------+------+------+------|   {  |       |  }   |------+------+------+------+------+------------|
  * | (   LShift |   Z  |   X  |   C  |   V  |   B  |      |       |      |   N  |   M  |   ,  |   .  |  /   | )   RShift |
  * `------------+------+------+------+------+-------------'       `-------------+------+------+------+------+------------'
@@ -62,25 +62,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(
 
 // left hand
- KC_ESC    ,KC_1   ,KC_2   ,KC_3   ,KC_4  ,KC_5  ,F(LSpec)
-,F(LMdia)  ,KC_Q   ,KC_W   ,KC_E   ,KC_R  ,KC_T  ,KC_LBRC
-,CTL_T(KC_ESC) ,KC_A ,KC_S ,KC_D   ,KC_F  ,KC_G
-,KC_LSPO   ,KC_Z   ,KC_X   ,KC_C   ,KC_V  ,KC_B  ,LSFT(KC_LBRC)
-,M(LSymb)  ,KC_MEH ,KC_HYPR,KC_LALT,KC_LGUI
-                                         ,KC_HOME,KC_END
-                                                 ,KC_PGUP
-                                 ,KC_BSPC,KC_DEL ,KC_PGDN
-                                                                  // right hand
-                                                                 ,KC_6   ,KC_7   ,KC_8   ,KC_9    ,KC_0    ,KC_MINS  ,F(RSpec)
-                                                                 ,KC_RBRC ,KC_Y   ,KC_U   ,KC_I   ,KC_O    ,KC_P     ,F(RMdia)
-                                                                          ,KC_H   ,KC_J   ,KC_K   ,KC_L    ,KC_SCLN  ,F(RSymb)
-                                                            ,LSFT(KC_RBRC),KC_N   ,KC_M   ,KC_COMM,KC_DOT  ,KC_SLSH  ,KC_RSPC
-                                                                                  //,KC_RGUI,KC_RALT,KC_HYPR ,KC_MEH   ,KC_RCTL
-                                                                                  ,KC_LEFT,KC_DOWN,KC_UP   ,KC_RIGHT ,KC_RCTL
-                                                                 ,KC_RGUI ,KC_MEH
-                                                                 ,KC_HYPR
-                                                                 ,KC_ENT ,KC_ENT ,KC_SPC
+KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5, F(LSpec),
+F(LMdia), KC_Q,   KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC,
+KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,
+KC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LSFT(KC_LBRC),
+M(LSymb), KC_MEH, KC_HYPR, KC_LALT, KC_LGUI,
+                                                     KC_HOME, KC_END,
+                                                              KC_PGUP,
+                                             KC_BSPC, KC_DEL, KC_PGDN,
 
+                                                                  // right hand
+        KC_6,          KC_7,    KC_8,     KC_9,     KC_0,    KC_MINS,  F(RSpec),
+        KC_RBRC,       KC_Y,    KC_U,     KC_I,     KC_O,    KC_P,     F(RMdia),
+                    KC_H,    KC_J,     KC_K,     KC_L,    KC_SCLN,  F(RSymb),
+        LSFT(KC_RBRC), KC_N,    KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSPC,
+                                KC_LEFT,  KC_DOWN,   KC_UP,  KC_RIGHT, KC_RCTL,
+        KC_RGUI,  KC_MEH,
+        KC_HYPR,
+        KC_ENT,   KC_ENT,  KC_SPC
+
+//,KC_RGUI,KC_RALT,KC_HYPR ,KC_MEH   ,KC_RCTL,
 ),
 
 /******* Symbols Layer *************************************************************************************************
