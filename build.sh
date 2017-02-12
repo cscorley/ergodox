da154d8f712c9d41bf8173b738e53c1c2f459755
Christopher Corley <cscorley@ua.edu>
2017-02-12 13:30:40 -0500

    fullpath build instructions

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-make && cp ../../../../.build /media/sf_shared -R > /dev/null
+#!/bin/bash
+
+
+qmk="${HOME}/git/qmk_firmware"
+c=`pwd`
+cd ${qmk} && git up
+cd ${c} && git up
+
+make && cp ${qmk}/.build /media/sf_shared -R > /dev/null
